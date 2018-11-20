9aa29d169ee455ca8aa25d487d63c5f96c5cf5a5
Werebearguy <darkhandrogue@gmail.com>
2018-11-20 13:41:13 -0500

    Create pet_AlienHornet_01.cs

---
+++ Projectiles/Pets/pet_AlienHornet_01.cs
@@ -0,0 +1,62 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Harblesnargits_Mod_01.Projectiles.Pets
+{
+	public class pet_AlienHornet_01 : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Alien Hornet");
+			Main.projFrames[projectile.type] = 4;
+			Main.projPet[projectile.type] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.CloneDefaults(ProjectileID.BabyHornet);
+			aiType = ProjectileID.BabyHornet;
+			projectile.width = 38;
+			projectile.height = 36;
+			projectile.alpha = 0;
+		}
+
+			public override void PostDraw(SpriteBatch spriteBatch, Color drawColor)
+				{
+					Texture2D texture = mod.GetTexture("Projectiles/Pets/pet_AlienHornet_01_glowmask");
+					Vector2 drawPos = projectile.position - Main.screenPosition;
+					Rectangle frame = new Rectangle(0, 0, texture.Width, texture.Height / 4);
+					frame.Y = (int)projectile.frameCounter % 60;
+					if (frame.Y > 24)
+						{
+							frame.Y = 24;
+						}
+					frame.Y *= projectile.height;
+					spriteBatch.Draw(texture, drawPos, frame, Color.White * 0.7f, 0f, Vector2.Zero, 1f, projectile.spriteDirection == -1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 0f);
+			}
+		
+		public override bool PreAI()
+		{
+			Player player = Main.player[projectile.owner];
+			player.hornet = false; // Relic from aiType
+			return true;
+		}
+
+		public override void AI()
+		{
+			Player player = Main.player[projectile.owner];
+			MyPlayer modPlayer = player.GetModPlayer<MyPlayer>(mod);
+			if (player.dead)
+			{
+				modPlayer.pet_AlienHornet_01 = false;
+			}
+			if (modPlayer.pet_AlienHornet_01)
+			{
+				projectile.timeLeft = 2;
+			}
+		}
+	}
+}
