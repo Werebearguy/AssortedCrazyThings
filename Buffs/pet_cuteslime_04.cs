5b84686a89864457cf576c58d41e9d427ff0d611
Werebearguy <darkhandrogue@gmail.com>
2018-11-27 14:45:06 -0500

    Add files via upload

--- Buffs/pet_cuteslime_04.cs
+++ Buffs/pet_cuteslime_04.cs
@@ -4,24 +4,23 @@ using Terraria.ModLoader;
 namespace Harblesnargits_Mod_01.Buffs
 {
 	public class pet_cuteslime_04 : ModBuff
-	{
-		public override void SetDefaults()
 		{
-			DisplayName.SetDefault("Cute Slime");
-			Description.SetDefault("A cute black slime girl is following you.");
-			Main.buffNoTimeDisplay[Type] = true;
-			Main.vanityPet[Type] = true;
+			public override void SetDefaults()
+				{
+					DisplayName.SetDefault("Cute Slime");
+					Description.SetDefault("A cute black slime girl is following you.");
+					Main.buffNoTimeDisplay[Type] = true;
+					Main.vanityPet[Type] = true;
+				}
+			public override void Update(Player player, ref int buffIndex)
+				{
+					player.buffTime[buffIndex] = 18000;
+					player.GetModPlayer<MyPlayer>(mod).pet_cuteslime_04 = true;
+					bool petProjectileNotSpawned = player.ownedProjectileCounts[mod.ProjectileType("pet_cuteslime_04")] <= 0;
+					if (petProjectileNotSpawned && player.whoAmI == Main.myPlayer)
+						{
+							Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, mod.ProjectileType("pet_cuteslime_04"), 0, 0f, player.whoAmI, 0f, 0f);
+						}
+				}
 		}
-
-		public override void Update(Player player, ref int buffIndex)
-		{
-			player.buffTime[buffIndex] = 18000;
-            player.GetModPlayer<MyPlayer>(mod).pet_cuteslime_04 = true;
-			bool petProjectileNotSpawned = player.ownedProjectileCounts[mod.ProjectileType("pet_cuteslime_04")] <= 0;
-			if (petProjectileNotSpawned && player.whoAmI == Main.myPlayer)
-			{
-				Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, mod.ProjectileType("pet_cuteslime_04"), 0, 0f, player.whoAmI, 0f, 0f);
-			}
-		}
-	}
 }
