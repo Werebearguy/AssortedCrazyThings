775d861d82fe2714261fff4931f7dcabad023ad9
Werebearguy <darkhandrogue@gmail.com>
2018-11-27 15:01:47 -0500

    Add files via upload

--- Items/Fun/ammo_endlessammo_01.cs
+++ Items/Fun/ammo_endlessammo_01.cs
@@ -13,27 +13,25 @@ using Terraria.Utilities;
 namespace Harblesnargits_Mod_01.Items.Fun
 {
 	public class ammo_endlessammo_01 : ModItem
-	{
-		public override void SetStaticDefaults()
 		{
-			DisplayName.SetDefault("Infinity Rocket");
-			Tooltip.SetDefault("It seriously never ends!");
+			public override void SetStaticDefaults()
+				{
+					DisplayName.SetDefault("Infinity Rocket");
+					Tooltip.SetDefault("It seriously never ends!");
+				}
+			public override void SetDefaults()
+				{
+					item.CloneDefaults(ItemID.EndlessMusketPouch);
+					item.ammo = AmmoID.Rocket;
+					item.rare = -11;
+				}
+			public override void AddRecipes()
+				{
+					ModRecipe recipe = new ModRecipe(mod);
+					recipe.AddIngredient(ItemID.RocketI, 3996);
+					recipe.AddTile(TileID.CrystalBall);  //WorkBenches, Anvils, MythrilAnvil, Furnaces, DemonAltar, or TinkerersWorkbench
+					recipe.SetResult(this);
+					recipe.AddRecipe();
+				}
 		}
-		
-		public override void SetDefaults()
-		{
-			item.CloneDefaults(ItemID.EndlessMusketPouch);
-			item.ammo = AmmoID.Rocket;
-			item.rare = -11;
-		}
-
-		public override void AddRecipes()
-		{
-			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.RocketI, 3996);
-            recipe.AddTile(TileID.CrystalBall);  //WorkBenches, Anvils, MythrilAnvil, Furnaces, DemonAltar, or TinkerersWorkbench
-			recipe.SetResult(this);
-			recipe.AddRecipe();
-		}
-	}
 }
--- Items/Fun/ammo_endlessammo_02.cs
+++ Items/Fun/ammo_endlessammo_02.cs
@@ -5,31 +5,28 @@ using Terraria.ModLoader;
 namespace Harblesnargits_Mod_01.Items.Fun
 {
 	public class ammo_endlessammo_02 : ModItem
-	{
-		public override void SetStaticDefaults()
 		{
-			DisplayName.SetDefault("Star Forge");
-			Tooltip.SetDefault("An endless supply of the cosmos");
+			public override void SetStaticDefaults()
+				{
+					DisplayName.SetDefault("Star Forge");
+					Tooltip.SetDefault("An endless supply of the cosmos");
+				}
+			public override void SetDefaults()
+				{
+					item.CloneDefaults(ItemID.FallenStar);
+					item.maxStack = 1;
+					item.consumable = false;
+					item.value = 0;
+					item.rare = -11;
+					item.shoot = mod.ProjectileType("proj_star_01");   //The projectile shoot when your weapon using this ammo
+				}
+			public override void AddRecipes()
+				{
+					ModRecipe recipe = new ModRecipe(mod);
+					recipe.AddIngredient(ItemID.FallenStar, 3996);
+					recipe.AddTile(TileID.CrystalBall);  //WorkBenches, Anvils, MythrilAnvil, Furnaces, DemonAltar, or TinkerersWorkbench
+					recipe.SetResult(this);
+					recipe.AddRecipe();
+				}
 		}
-
-		public override void SetDefaults()
-		{
-			item.CloneDefaults(ItemID.FallenStar);
-			item.maxStack = 1;
-			item.consumable = false;
-			item.value = 0;
-			item.rare = -11;
-			item.shoot = mod.ProjectileType("proj_star_01");   //The projectile shoot when your weapon using this ammo
-									   //Without this line, it won't actually fire stars.
-		}
-
-		public override void AddRecipes()
-		{
-			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.FallenStar, 3996);
-            recipe.AddTile(TileID.CrystalBall);  //WorkBenches, Anvils, MythrilAnvil, Furnaces, DemonAltar, or TinkerersWorkbench
-			recipe.SetResult(this);
-			recipe.AddRecipe();
-		}
-	}
 }
