dc3adbbcd5504abe288b833afccf907351f8742d
direwolf240 <amakejk1@web.de>
2018-11-28 19:33:55 +0100

    req 13

--- NPCs/enemy_crabchest_01.cs
+++ NPCs/enemy_crabchest_01.cs
@@ -7,42 +7,57 @@ namespace Harblesnargits_Mod_01.NPCs
 	public class enemy_crabchest_01 : ModNPC
 	{
 		public override void SetStaticDefaults()
-			{
-				DisplayName.SetDefault("Walking Chest");
-				Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.Crab];
-			}
+		{
+			DisplayName.SetDefault("Walking Chest");
+			Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.Crab];
+		}
+
 		public override void SetDefaults()
-			{
-				npc.width = 44;
-				npc.height = 34;
-				npc.damage = 0;
-				npc.defense = 10;
-				npc.lifeMax = 40;
-				npc.HitSound = SoundID.NPCHit1;
-				npc.DeathSound = SoundID.NPCDeath1;
-				npc.value = 75f;
-				npc.knockBackResist = 0.5f;
-				npc.aiStyle = 7;
-				aiType = NPCID.Crab;
-				animationType = NPCID.Crab;
-			}
+		{
+			npc.width = 44;
+			npc.height = 34;
+			npc.damage = 0;
+			npc.defense = 10;
+			npc.lifeMax = 40;
+			npc.HitSound = SoundID.NPCHit1;
+			npc.DeathSound = SoundID.NPCDeath1;
+			npc.value = 75f;
+			npc.knockBackResist = 0.5f;
+			npc.aiStyle = 7;
+			aiType = NPCID.Crab;
+			animationType = NPCID.Crab;
+		}
+
 		public override float SpawnChance(NPCSpawnInfo spawnInfo)
-			{
-				return SpawnCondition.Underground.Chance * 0.025f;
-			}
+		{
+			return SpawnCondition.Underground.Chance * 0.025f;
+		}
+
         public override void NPCLoot()
-			{
-				{
-					Item.NewItem(npc.getRect(), ItemID.Chest);
-				}
-			}
+		{
+            Item.NewItem(npc.getRect(), ItemID.Chest);
+		}
+
 		public override void HitEffect(int hitDirection, double damage)
-			{
+		{
+			if (npc.life <= 0)
 				{
-					if (npc.life <= 0)
-						{
-						}
 				}
-			}
-	}
+        }
+
+        public override void PostAI()
+        {
+            if (Main.dayTime)
+            {
+                if (npc.velocity.X > 0 || npc.velocity.X < 0)
+                {
+                    npc.velocity.X = 0;
+                }
+                if (npc.velocity.Y < 0)
+                {
+                    npc.velocity.Y = 0;
+                }
+            }
+        }
+    }
 }
