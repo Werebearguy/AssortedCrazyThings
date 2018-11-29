ae8828be2f718735b126445a149d0c1a0945b6b8
direwolf240 <amakejk1@web.de>
2018-11-29 16:41:39 +0100

    req 8

--- Projectiles/Weapons/proj_fire_01.cs
+++ Projectiles/Weapons/proj_fire_01.cs
@@ -8,17 +8,26 @@ namespace Harblesnargits_Mod_01.Projectiles.Weapons
     public class proj_fire_01 : ModProjectile
     {
         public override void SetStaticDefaults()
-		{
-            DisplayName.SetDefault("This is supposed to be green, not orange");
-		}
-		
+        {
+            DisplayName.SetDefault("Handle with care");
+        }
+
         public override void SetDefaults()
         {
 			projectile.CloneDefaults(ProjectileID.EyeFire);
 			projectile.aiStyle = 23;
 			projectile.friendly = true;
 			projectile.hostile = false;
+            //adding this makes the game select the AI that is meant for the Spazmatism cursed fire ai
+            aiType = ProjectileID.EyeFire;
         }
-		
+
+        public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
+        {
+            if (Main.rand.NextFloat() >= .66f)
+            {
+                target.AddBuff(BuffID.CursedInferno, 180); //three full seconds, 33% chance
+            }
+        }
     }
 }
