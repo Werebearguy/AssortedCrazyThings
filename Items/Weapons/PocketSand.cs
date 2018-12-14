d4c08990ce9ef7f74868ad5f9097f92dee3971f6
direwolf420 <amakejk1@web.de>
2018-12-14 14:25:57 +0100

    pocket sand first implementation

--- Items/Weapons/PocketSand.cs
+++ Items/Weapons/PocketSand.cs
@@ -1,3 +1,4 @@
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -10,10 +11,11 @@ namespace AssortedCrazyThings.Items.Weapons
         {
 			item.CloneDefaults(ItemID.ThrowingKnife);
             item.damage = 1;
-            item.useTime = 10;
-            item.shootSpeed = 10f;
+            item.useTime = 35;
+            item.shootSpeed = 4.5f;
             item.shoot = mod.ProjectileType("PocketSand");
             item.useAnimation = 35;
+            item.autoReuse = true;
 			item.value = 0;
             item.rare = -11;
 			item.noUseGraphic = true;
@@ -21,16 +23,37 @@ namespace AssortedCrazyThings.Items.Weapons
 
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("{Pocket Sand");
+            DisplayName.SetDefault("Pocket Sand");
             Tooltip.SetDefault("Throw a clump of sand at an enemy to confuse it.");
         }
 
-		public override void AddRecipes()
-		{
-			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.SandBlock, 1);
-			recipe.SetResult(this);
-			recipe.AddRecipe();
-		}
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.SandBlock, 1);
+            recipe.SetResult(this, 10);
+            recipe.AddRecipe();
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            if (base.Shoot(player, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack)) //spawns an accurate projectile once
+            {
+                for (int i = 0; i < 2; i++) //spawn two more with random velocity if first one is actually spawned
+                {
+                    Vector2 cm = new Vector2(Main.MouseWorld.X - player.Center.X, Main.MouseWorld.Y - player.Center.Y);
+                    float randx = Main.rand.NextFloat(0.8f, 1.2f);
+                    float randx2 = Main.rand.NextFloat(-1.1f, 1.1f);
+                    float randy = Main.rand.NextFloat(0.8f, 1.2f);
+                    float bobandy = Main.rand.NextFloat(-1.1f, 1.1f);
+                    float velox = ((cm.X * item.shootSpeed * randx) / cm.Length()) + randx2; //first rand makes it so it has different velocity factor (how far it flies)
+                    float veloy = ((cm.Y * item.shootSpeed * randy) / cm.Length()) + bobandy; //second rand is a kinda offset used mainly for when shooting vertically or horizontally
+                    Vector2 velo = new Vector2(velox, veloy);
+                    Projectile.NewProjectile(position, velo, type, damage, knockBack);
+                }
+                return true;
+            }
+            return false;
+        }
 	}
 }
--- Projectiles/Weapons/PocketSand.cs
+++ Projectiles/Weapons/PocketSand.cs
@@ -7,24 +7,60 @@ namespace AssortedCrazyThings.Projectiles.Weapons
 {
     public class PocketSand : ModProjectile
     {
+        private static int ProjectileTimeLeft = 30;
+
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Surprise");
+            DisplayName.SetDefault("Pocket Sand");
         }
 
         public override void SetDefaults()
         {
-			projectile.CloneDefaults(ProjectileID.PurificationPowder);
-			projectile.aiStyle = 6;
+			projectile.CloneDefaults(ProjectileID.ThrowingKnife);
+			projectile.aiStyle = 2; //6 for powder, 2 for throwing knife
+            projectile.height = 20;
+            projectile.width = 20;
+            projectile.timeLeft = ProjectileTimeLeft;
+            projectile.tileCollide = true;
 			projectile.friendly = true;
 			projectile.hostile = false;
         }
 
         public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
         {
-            if (Main.rand.NextFloat() >= .66f)
+            if (Main.rand.NextFloat() >= .50f)
             {
-                target.AddBuff(BuffID.Confused, 120); //two full seconds, 33% chance
+                target.AddBuff(BuffID.Confused, 90); //1 1/2 seconds, 50% chance
+            }
+        }
+
+        private void SpawnSandDust(int type, Color color, Rectangle hitbox, Player player, float velox)
+        {
+            //6 is the default fire particle type
+            //the thick no-outline one that spreads
+            int dustid = Dust.NewDust(new Vector2((float)hitbox.X, (float)(hitbox.Y - player.height / 2)), hitbox.Width, hitbox.Height, type, player.velocity.X * 0.2f + (Main.rand.NextBool() ? velox : -velox), player.velocity.Y * 0.2f, 150, color, 1f);
+            Main.dust[dustid].noGravity = false;
+            Main.dust[dustid].fadeIn = 0.8f;
+            //the outline one 
+            int dustid2 = Dust.NewDust(new Vector2((float)hitbox.X, (float)(hitbox.Y - player.height / 2)), hitbox.Width, hitbox.Height, 32, player.velocity.X * 0.2f, player.velocity.Y * 0.2f, 200, Color.White, 0.8f);
+            Main.dust[dustid2].noGravity = true;
+        }
+
+        public override void ModifyDamageHitbox(ref Rectangle hitbox)
+        {
+            //increase hitbox used in colliding with NPCs while projectile life depletes
+            hitbox.Width += (int)((ProjectileTimeLeft - projectile.timeLeft) * 1.5f);
+            hitbox.Height += (int)((ProjectileTimeLeft - projectile.timeLeft) * 1.5f);
+            hitbox.X -= (int)((ProjectileTimeLeft - projectile.timeLeft) * 1.5f / 2f);
+            hitbox.Y -= (int)((ProjectileTimeLeft - projectile.timeLeft) * 1.5f / 2f);
+        }
+
+        public override void PostAI()
+        {
+            //dont spawn the dust instantly when projectile spawns, give it 1/12th of a second
+            if(projectile.timeLeft < ProjectileTimeLeft - 5)
+            {
+                SpawnSandDust(102, Color.White, projectile.Hitbox, Main.player[projectile.owner], (ProjectileTimeLeft - projectile.timeLeft) * 0.1f);
             }
         }
     }
