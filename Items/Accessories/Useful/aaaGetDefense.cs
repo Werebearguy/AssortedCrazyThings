2395d7369d5b13597ac76da8b781f4279e336747
direwolf420 <amakejk1@web.de>
2018-12-31 23:04:05 +0100

    implement high defense when low item

---
+++ Items/Accessories/Useful/aaaGetDefense.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace AssortedCrazyThings.Items.Accessories.Useful
+{
+    class aaaGetDefense : ModItem //todo proper name
+    {
+        public override string Texture
+        {
+            get
+            {
+                return "AssortedCrazyThings/Items/Accessories/Useful/EverburningCursedCandle"; //temp, delete this when proper texture exists
+            }
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("aaaGetDefense"); //todo proper name
+            Tooltip.SetDefault("You get very high defense when health is dangerously low");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 24;
+            item.height = 22;
+            item.value = Item.buyPrice(0, 20, 0, 0);
+            item.rare = -11;
+            item.accessory = true;
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            AssPlayer mPlayer = Main.LocalPlayer.GetModPlayer<AssPlayer>(mod);
+            if (mPlayer.canTeleportHomeWhenLow)
+            {
+                //the first string is irrelevant, its never used anywhere, basically just a name for that line
+                tooltips.Add(new TooltipLine(mod, "CanUse", "Ready to use"));
+            }
+            else
+            {
+                string timeName;
+                if(mPlayer.getDefenseTimer > 60) //more than 1 minute
+                {
+                    if (mPlayer.getDefenseTimer > 90) //more than 1:30 minutes because of round
+                    {
+                        timeName = " minutes";
+                    }
+                    else
+                    {
+                        timeName = " minute";
+                    }
+                    tooltips.Add(new TooltipLine(mod, "UsableIn", "Ready again in " + Math.Round(mPlayer.getDefenseTimer / 60f) + timeName));
+                }
+                else
+                {
+                    if (mPlayer.teleportHomeWhenLowTimer > 1) //more than 1 second
+                    {
+                        timeName = " seconds";
+                    }
+                    else
+                    {
+                        timeName = " second";
+                    }
+                    tooltips.Add(new TooltipLine(mod, "UsableIn", "Ready again in " + mPlayer.getDefenseTimer + timeName));
+                }
+            }
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<AssPlayer>().getDefenseWhenLow = true;
+        }
+
+        //TODO recipe
+        //TODO set the time in seconds that the item stays in cooldown in AssPlayer.cs, here:
+        // private const short TeleportHomeWhenLowTimerMax = 1200; == 20 minutes
+    }
+}
