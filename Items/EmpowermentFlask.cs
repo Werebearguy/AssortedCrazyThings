b2ed529582727dbb6796d710bb39c304fa48900c
direwolf420 <amakejk1@web.de>
2019-01-09 14:52:57 +0100

    rename Soul Flask

--- Items/EmpowermentFlask.cs
+++ Items/EmpowermentFlask.cs
@@ -5,11 +5,11 @@ using Terraria.ModLoader;
 
 namespace AssortedCrazyThings.Items
 {
-    class SoulFlask: ModItem
+    class EmpowermentFlask: ModItem
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Soul Flask");
+            DisplayName.SetDefault("Empowerment Flask");
             Tooltip.SetDefault("Incrementally increases damage dealt over time"
                 + "\nBonus resets upon taking damage");
         }
@@ -25,10 +25,20 @@ namespace AssortedCrazyThings.Items
             item.UseSound = SoundID.Item3;
             item.maxStack = 30;
             item.consumable = true;
-            item.buffTime = 5400;
+            item.buffTime = 5400; //one and a half minutes
             item.buffType = mod.BuffType<EmpoweringBuff>();
             item.rare = -11;
             item.value = Item.buyPrice(gold: 1);
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(mod.ItemType<CaughtDungeonSoulAwakened>(), 1);
+            recipe.AddIngredient(ItemID.Bone, 10);
+            recipe.AddTile(TileID.Bottles);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
         }
     }
 }
