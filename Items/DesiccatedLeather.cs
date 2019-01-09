1ec05a2e25562ab4d01c2de12c8bd31604a3ba93
Werebearguy <darkhandrogue@gmail.com>
2019-01-09 02:03:21 -0500

    Added bird leather, touched up some small things, made To-do list look nicer

---
+++ Items/DesiccatedLeather.cs
@@ -0,0 +1,20 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AssortedCrazyThings.Items
+{
+	public class DesiccatedLeather : ModItem
+	{
+        public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Desiccated Leather");
+			Tooltip.SetDefault("It's dry, sticky, and smells horrible.");
+		}
+		public override void SetDefaults()
+		{
+			item.CloneDefaults(ItemID.Silk);
+			item.rare = -11;
+		}
+	}
+}
