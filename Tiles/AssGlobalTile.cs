17ac585875b55882b509c64bdd420bcb7d231b90
direwolf420 <amakejk1@web.de>
2019-01-30 22:49:50 +0100

    add temporary drop to fake sword shrine

---
+++ Tiles/AssGlobalTile.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using AssortedCrazyThings.Items;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AssortedCrazyThings.Tiles
+{
+    class AssGlobalTile : GlobalTile
+    {
+        public override bool Drop(int i, int j, int type)
+        {
+            if(type == 186) //fake sword shrine
+            {
+                Item.NewItem(i * 16, j * 16, 32, 32, mod.ItemType<CaughtDungeonSoul>());
+            }
+            return true;
+        }
+    }
+}
