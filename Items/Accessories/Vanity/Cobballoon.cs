bb78c9a27a4c9bc58bfa3f7b0e24e24e1e496838
Werebearguy <darkhandrogue@gmail.com>
2019-02-02 00:34:47 -0500

    Added Cobballoon, added purple cute slime NoHair sprite, added item and tile sprite for slime rain item

---
+++ Items/Accessories/Vanity/Cobballoon.cs
@@ -0,0 +1,34 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AssortedCrazyThings.Items.Accessories.Vanity
+{
+	[AutoloadEquip(EquipType.Balloon)]
+	public class Cobballoon : ModItem
+		{
+			public override void SetStaticDefaults()
+				{
+					DisplayName.SetDefault("Cobballoon");
+					Tooltip.SetDefault("A balloon made of rocks, don't ask how");
+				}
+			public override void SetDefaults()
+				{
+					item.width = 18;
+					item.height = 32;
+					item.value = 0;
+					item.rare = -11;
+					item.accessory = true;
+				}
+			public override void AddRecipes()
+				{
+					ModRecipe recipe = new ModRecipe(mod);
+					recipe.AddIngredient(ItemID.StoneBlock, 25);
+					recipe.AddIngredient(ItemID.WhiteString, 1);
+					recipe.AddIngredient(ItemID.ShinyRedBalloon, 1);
+					recipe.AddTile(TileID.TinkerersWorkbench);
+					recipe.SetResult(this);
+					recipe.AddRecipe();
+				}
+		}
+}
