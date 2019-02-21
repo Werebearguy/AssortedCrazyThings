31f92fbbbe0df8176c3304c3570aa7373a1bd025
direwolf420 <amakejk1@web.de>
2019-02-21 23:09:28 +0100

    visuals for rainbow and illuminant pet slime

--- Projectiles/Pets/IlluminantSlimeProj.cs
+++ Projectiles/Pets/IlluminantSlimeProj.cs
@@ -1,4 +1,7 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
+using Terraria.ID;
 
 namespace AssortedCrazyThings.Projectiles.Pets
 {
@@ -12,6 +15,8 @@ namespace AssortedCrazyThings.Projectiles.Pets
             Main.projPet[projectile.type] = true;
             drawOffsetX = 0;
             drawOriginOffsetY = 4;
+            ProjectileID.Sets.TrailingMode[projectile.type] = 0;
+            ProjectileID.Sets.TrailCacheLength[projectile.type] = 8;
         }
 
         public override void MoreSetDefaults()
@@ -36,6 +41,31 @@ namespace AssortedCrazyThings.Projectiles.Pets
                 projectile.timeLeft = 2;
             }
             return true;
+        }
+
+        public override void PostDraw(SpriteBatch spriteBatch, Color drawColor)
+        {
+            Texture2D image = mod.GetTexture("Projectiles/Pets/IlluminantSlimeProj_Glowmask");
+            Rectangle bounds = new Rectangle
+            {
+                X = 0,
+                Y = projectile.frame,
+                Width = image.Bounds.Width,
+                Height = image.Bounds.Height / 6
+            };
+            bounds.Y *= bounds.Height; //cause proj.frame only contains the frame number
+
+            SpriteEffects effect = projectile.spriteDirection == -1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+            Vector2 drawOrigin = new Vector2(projectile.width * 0.5f, projectile.height * 0.5f + drawOriginOffsetY);
+            //the higher the k, the older the position
+            //Length is implicitely set in TrailCacheLength up there
+            //start from half the length so the origninal sprite isnt super blurred
+            for (int k = projectile.oldPos.Length - 1; k >= 0; k--)
+            {
+                Vector2 drawPos = projectile.oldPos[k] - Main.screenPosition + drawOrigin + new Vector2(0f, projectile.gfxOffY);
+                Color color = projectile.GetAlpha(Color.White) * ((projectile.oldPos.Length - k) / (1f * projectile.oldPos.Length));
+                spriteBatch.Draw(image, drawPos, bounds, color, projectile.oldRot[k], bounds.Size() / 2, projectile.scale, effect, 0f);
+            }
         }
     }
 }
--- Projectiles/Pets/RainbowSlimeProj.cs
+++ Projectiles/Pets/RainbowSlimeProj.cs
@@ -1,3 +1,5 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 
 namespace AssortedCrazyThings.Projectiles.Pets
@@ -19,7 +21,6 @@ namespace AssortedCrazyThings.Projectiles.Pets
             //used to set dimensions and damage (if there is, defaults to 0)
             projectile.width = 52;
             projectile.height = 38;
-			projectile.alpha = 0;
 
             Damage = 0;
         }
@@ -36,6 +37,32 @@ namespace AssortedCrazyThings.Projectiles.Pets
                 projectile.timeLeft = 2;
             }
             return true;
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color drawColor)
+        {
+            Texture2D image = Main.projectileTexture[projectile.type];
+            Rectangle bounds = new Rectangle
+            {
+                X = 0,
+                Y = projectile.frame,
+                Width = image.Bounds.Width,
+                Height = image.Bounds.Height / 6
+            };
+            bounds.Y *= bounds.Height; //cause proj.frame only contains the frame number
+            Vector2 stupidOffset = new Vector2(0f, projectile.gfxOffY); //gfxoffY is for when the npc is on a slope or half brick
+            SpriteEffects effect = projectile.spriteDirection == -1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+            Vector2 drawOrigin = new Vector2(projectile.width * 0.5f, projectile.height * 0.5f + drawOriginOffsetY);
+            Vector2 drawPos = projectile.position - Main.screenPosition + drawOrigin + stupidOffset;
+
+            double cX = projectile.Center.X + drawOffsetX;
+            double cY = projectile.Center.Y + drawOriginOffsetY;
+            drawColor = Lighting.GetColor((int)(cX / 16), (int)(cY / 16), Main.DiscoColor * 1.2f);
+
+            Color color = drawColor * ((255 - projectile.alpha) / 255f);
+
+            spriteBatch.Draw(image, drawPos, bounds, color, projectile.rotation, bounds.Size() / 2, projectile.scale, effect, 0f);
+            return false;
         }
     }
 }
