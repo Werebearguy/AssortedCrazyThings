4975ab5fcb5b888894b93c9dcb5a1799ea6a781d
direwolf420 <amakejk1@web.de>
2019-04-11 14:21:40 +0200

    pet cultist fixes

--- Projectiles/Pets/PetCultistProj.cs
+++ Projectiles/Pets/PetCultistProj.cs
@@ -6,19 +6,28 @@ namespace AssortedCrazyThings.Projectiles.Pets
 {
     public class PetCultistProj : ModProjectile
     {
+        public override string Texture
+        {
+            get
+            {
+                return "AssortedCrazyThings/Projectiles/Pets/PetCultistProj_0"; //temp
+            }
+        }
+
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Tiny Cultist");
             Main.projFrames[projectile.type] = 4;
             Main.projPet[projectile.type] = true;
+            ProjectileID.Sets.LightPet[projectile.type] = true;
         }
 
         public override void SetDefaults()
         {
             projectile.CloneDefaults(ProjectileID.ZephyrFish);
             projectile.aiStyle = -1;
-            projectile.width = 32;
-            projectile.height = 32;
+            projectile.width = 20;
+            projectile.height = 26;
             projectile.tileCollide = false;
         }
 
@@ -34,7 +43,7 @@ namespace AssortedCrazyThings.Projectiles.Pets
             {
                 projectile.timeLeft = 2;
             }
-            AssAI.FlickerwickPetAI(projectile, lightPet: false, lightDust: false, reverseSide: true, vanityPet: true, veloSpeed: 0.5f, offsetX: 20f, offsetY: -60f);
+            AssAI.FlickerwickPetAI(projectile, lightPet: false, lightDust: false, reverseSide: true, vanityPet: true, veloXToRotationFactor: 0.5f, offsetX: 16f, offsetY: (player.statLife < player.statLifeMax2 / 2)? -26f: 2f);
             AssAI.FlickerwickPetDraw(projectile, 8, 8);
         }
     }
