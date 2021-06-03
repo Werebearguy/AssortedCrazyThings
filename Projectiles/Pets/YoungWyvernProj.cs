9a9e5e18493c71e4c0c1576804c930a868ce855a
direwolf420 <amakejk1@web.de>
2021-06-03 16:33:44 +0200

    add young wyvern resprite

--- Projectiles/Pets/YoungWyvernProj.cs
+++ Projectiles/Pets/YoungWyvernProj.cs
@@ -1,6 +1,7 @@
 using AssortedCrazyThings.Base;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -9,6 +10,11 @@ namespace AssortedCrazyThings.Projectiles.Pets
 {
     public class YoungWyvernProj : ModProjectile
     {
+        public int frame = 0;
+        public int frameCounter = 0;
+
+        public bool InAir => Projectile.ai[0] != 0f;
+
         public override string Texture
         {
             get
@@ -20,14 +26,16 @@ namespace AssortedCrazyThings.Projectiles.Pets
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Young Wyvern");
-            Main.projFrames[Projectile.type] = 11;
+            Main.projFrames[Projectile.type] = 9;
             Main.projPet[Projectile.type] = true;
-            DrawOffsetX = -12;
         }
 
         public override void SetDefaults()
         {
             Projectile.CloneDefaults(ProjectileID.BlackCat);
+            DrawOffsetX = -6;
+            Projectile.width = 44;
+            Projectile.height = 30;
             AIType = ProjectileID.BlackCat;
         }
 
@@ -50,12 +58,120 @@ namespace AssortedCrazyThings.Projectiles.Pets
             {
                 Projectile.timeLeft = 2;
             }
+
+            //BLACK CAT
+            //bool flag11 = Projectile.position.X - Projectile.oldPosition.X == 0f;
+            //if (InAir)
+            //{
+            //    rotation = base.Projectile.velocity.X * 0.05f;
+            //    frameCounter++;
+            //    if (frameCounter >= 6)
+            //    {
+            //        frame++;
+            //        frameCounter = 0;
+            //    }
+
+            //    if (frame > 10)
+            //        frame = 6;
+
+            //    if (frame < 6)
+            //        frame = 6;
+            //}
+            //else
+            //{
+            //    if (base.Projectile.velocity.Y >= 0f && (double)base.Projectile.velocity.Y <= 0.8)
+            //    {
+            //        if (flag11)
+            //        {
+            //            frame = 0;
+            //            frameCounter = 0;
+            //        }
+            //        else if ((double)base.Projectile.velocity.X < -0.8 || (double)base.Projectile.velocity.X > 0.8)
+            //        {
+            //            frameCounter += (int)Math.Abs(base.Projectile.velocity.X);
+            //            frameCounter++;
+            //            if (frameCounter > 8)
+            //            {
+            //                frame++;
+            //                frameCounter = 0;
+            //            }
+
+            //            if (frame > 5)
+            //                frame = 2;
+            //        }
+            //        else
+            //        {
+            //            frame = 0;
+            //            frameCounter = 0;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        frameCounter = 0;
+            //        frame = 1;
+            //    }
+            //}
+        }
+
+        public override void PostAI()
+        {
+            //Black cat AI values, so we have to check those
+            if (!InAir)
+            {
+                //DESERT TIGER
+                int lastFrame = 8; //frame #8 is spinning, frame #9 is blank
+
+                //if (fancy desert tigers)
+                //    lastFrame = 10;
+
+                //Projectile.rotation = 0f;
+                if (Projectile.velocity.Y >= 0f && Projectile.velocity.Y <= 0.8f)
+                {
+                    if (Projectile.position.X - Projectile.oldPosition.X == 0f)
+                    {
+                        frame = 0;
+                        frameCounter = 0;
+                    }
+                    else if (Math.Abs(Projectile.velocity.X) >= 0.5f)
+                    {
+                        frameCounter += (int)Math.Abs(Projectile.velocity.X);
+                        frameCounter++;
+                        if (frameCounter > 10)
+                        {
+                            frame++;
+                            frameCounter = 0;
+                        }
+
+                        if (frame >= lastFrame || frame < 2)
+                            frame = 2;
+                    }
+                    else
+                    {
+                        frame = 0;
+                        frameCounter = 0;
+                    }
+                }
+                else if (Projectile.velocity.Y != 0f)
+                {
+                    frameCounter = 0;
+                    frame = 1;
+                    //if (fancy desert tigers)
+                    //    frame = 9;
+                }
+            }
+            else
+            {
+                frameCounter = 0;
+                frame = 1;
+            }
+
+            Projectile.frame = frame;
         }
 
         public override bool PreDraw(ref Color lightColor)
         {
             PetPlayer mPlayer = Projectile.GetOwner().GetModPlayer<PetPlayer>();
-            SpriteEffects effects = Projectile.spriteDirection == -1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+            SpriteEffects effects = Projectile.spriteDirection == 1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
             Texture2D image = Mod.GetTexture("Projectiles/Pets/YoungWyvernProj_" + mPlayer.youngWyvernType).Value;
             Rectangle bounds = new Rectangle
             {
@@ -66,7 +182,7 @@ namespace AssortedCrazyThings.Projectiles.Pets
             };
             bounds.Y *= bounds.Height; //cause proj.frame only contains the frame number
 
-            Vector2 stupidOffset = new Vector2(Projectile.width / 2, 23f + Projectile.gfxOffY);
+            Vector2 stupidOffset = new Vector2(Projectile.width / 2 + DrawOffsetX, Projectile.height / 2 + Projectile.gfxOffY + 4f);
 
             Main.spriteBatch.Draw(image, Projectile.position - Main.screenPosition + stupidOffset, bounds, lightColor, Projectile.rotation, bounds.Size() / 2, Projectile.scale, effects, 0f);
 
