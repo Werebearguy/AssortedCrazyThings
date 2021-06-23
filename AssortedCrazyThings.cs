eeebf5f2349f1ee126c1d3195001e1c7580610c8
direwolf420 <amakejk1@web.de>
2021-06-23 19:27:22 +0200

    Move ModCalls into a separate class

--- AssortedCrazyThings.cs
+++ AssortedCrazyThings.cs
@@ -1,16 +1,8 @@
 using AssortedCrazyThings.Base;
-using AssortedCrazyThings.Base.SlimeHugs;
-using AssortedCrazyThings.Buffs;
 using AssortedCrazyThings.Effects;
-using AssortedCrazyThings.Items;
-using AssortedCrazyThings.Items.Accessories.Useful;
-using AssortedCrazyThings.Items.PetAccessories;
 using AssortedCrazyThings.Items.Pets.CuteSlimes;
-using AssortedCrazyThings.Items.VanityArmor;
 using AssortedCrazyThings.Items.Weapons;
 using AssortedCrazyThings.NPCs.DungeonBird;
-using AssortedCrazyThings.Projectiles.Minions;
-using AssortedCrazyThings.Projectiles.Minions.CompanionDungeonSouls;
 using AssortedCrazyThings.Projectiles.Pets;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -211,84 +203,6 @@ namespace AssortedCrazyThings
                 ModContent.ProjectileType<PetDestroyerBody2>(),
                 ModContent.ProjectileType<PetDestroyerTail>()
                 };
-            }
-
-            //https://forums.terraria.org/index.php?threads/boss-checklist-in-game-progression-checklist.50668/
-            if (AConfigurationConfig.Instance.Bosses && ModLoader.TryGetMod("BossChecklist", out Mod bossChecklist))
-            {
-                //5.1f means just after skeletron
-                if (bossChecklist.Version >= new Version(1, 0))
-                {
-                    /*
-                     * "AddMiniBoss",
-                     * float progression,
-                     * int/List<int> miniBossNPCIDs,
-                     * Mod mod,
-                     * string minibossName,
-                     * Func<bool> downedMiniBoss,
-                     * int/List<int> SpawnItemIDs,
-                     * int/List<int> CollectionItemIDs,
-                     * int/List<int> LootItemIDs,
-                     * [string spawnInfo],
-                     * [string despawnMessage],
-                     * [string texture],
-                     * [string overrideHeadIconTexture],
-                     * [Func<bool> miniBossAvailable]
-                     */
-
-                    int summonItem = ModContent.ItemType<IdolOfDecay>();
-                    bossChecklist.Call(
-                        "AddMiniBoss",
-                        5.1f,
-                        ModContent.NPCType<Harvester>(),
-                        this,
-                        Harvester.name,
-                        (Func<bool>)(() => AssWorld.downedHarvester),
-                        summonItem,
-                        ModContent.ItemType<SoulHarvesterMask>(),
-                        new List<int>
-                        {
-                            summonItem,
-                            ModContent.ItemType<CaughtDungeonSoulFreed>(),
-                            ModContent.ItemType<SigilOfRetreat>(),
-                            ModContent.ItemType<SigilOfEmergency>(),
-                            ModContent.ItemType<SigilOfPainSuppression>(),
-                        },
-                        $"Use a [i:{summonItem}] in the dungeon after Skeletron has been defeated",
-                        null,
-                        $"{this.Name}/NPCs/DungeonBird/HarvesterPreview"
-                    );
-                }
-                else
-                {
-                    bossChecklist.Call("AddMiniBossWithInfo", Harvester.name, 5.1f, (Func<bool>)(() => AssWorld.downedHarvester), "Use a [i:" + ModContent.ItemType<IdolOfDecay>() + "] in the dungeon after Skeletron has been defeated");
-                }
-            }
-
-            if (ModLoader.TryGetMod("SummonersAssociation", out Mod summonersAssociation) && summonersAssociation.Version > new Version(0, 4, 1))
-            {
-                if (AConfigurationConfig.Instance.Bosses && AConfigurationConfig.Instance.Weapons)
-                {
-                    summonersAssociation.Call("AddMinionInfo", ModContent.ItemType<EverglowLantern>(), ModContent.BuffType<CompanionDungeonSoulMinionBuff>(), new List<int>
-                    {
-                        ModContent.ProjectileType<CompanionDungeonSoulPreWOFMinion>(),
-                    });
-                        summonersAssociation.Call("AddMinionInfo", ModContent.ItemType<EverhallowedLantern>(), ModContent.BuffType<CompanionDungeonSoulMinionBuff>(), new List<int>
-                    {
-                        ModContent.ProjectileType<CompanionDungeonSoulPostWOFMinion>(),
-                        ModContent.ProjectileType<CompanionDungeonSoulFrightMinion>(),
-                        ModContent.ProjectileType<CompanionDungeonSoulMightMinion>(),
-                        ModContent.ProjectileType<CompanionDungeonSoulSightMinion>()
-                    });
-                }
-
-                summonersAssociation.Call("AddMinionInfo", ModContent.ItemType<SlimeHandlerKnapsack>(), ModContent.BuffType<SlimePackMinionBuff>(), ModContent.ProjectileType<SlimePackMinion>());
-                List<int> drones = new List<int>();
-                foreach (var drone in DroneController.DataList)
-                {
-                    drones.Add(drone.ProjType);
-                }
-                summonersAssociation.Call("AddMinionInfo", ModContent.ItemType<DroneController>(), ModContent.BuffType<DroneControllerBuff>(), drones);
             }
         }
 
---
+++ Base/ModSupport/OtherModCalls.cs
@@ -0,0 +1,133 @@
+﻿using AssortedCrazyThings.Buffs;
+using AssortedCrazyThings.Items;
+using AssortedCrazyThings.Items.Accessories.Useful;
+using AssortedCrazyThings.Items.Placeable;
+using AssortedCrazyThings.Items.VanityArmor;
+using AssortedCrazyThings.Items.Weapons;
+using AssortedCrazyThings.NPCs.DungeonBird;
+using AssortedCrazyThings.Projectiles.Minions;
+using AssortedCrazyThings.Projectiles.Minions.CompanionDungeonSouls;
+using System;
+using System.Collections.Generic;
+using Terraria.ModLoader;
+
+namespace AssortedCrazyThings.Base.ModSupport
+{
+    [Autoload]
+    public class OtherModCalls : AssSystem
+    {
+        public override void PostSetupContent()
+        {
+            //https://forums.terraria.org/index.php?threads/boss-checklist-in-game-progression-checklist.50668/
+            if (AConfigurationConfig.Instance.Bosses && ModLoader.TryGetMod("BossChecklist", out Mod bossChecklist))
+            {
+                //5.1f means just after skeletron
+                if (bossChecklist.Version >= new Version(1, 0))
+                {
+                    /*
+                     * "AddMiniBoss",
+                     * float progression,
+                     * int/List<int> miniBossNPCIDs,
+                     * Mod mod,
+                     * string minibossName,
+                     * Func<bool> downedMiniBoss,
+                     * int/List<int> SpawnItemIDs,
+                     * int/List<int> CollectionItemIDs,
+                     * int/List<int> LootItemIDs,
+                     * [string spawnInfo],
+                     * [string despawnMessage],
+                     * [string texture],
+                     * [string overrideHeadIconTexture],
+                     * [Func<bool> miniBossAvailable]
+                     */
+
+                    int summonItem = ModContent.ItemType<IdolOfDecay>();
+
+                    List<int> collection = new List<int>();
+                    if (AConfigurationConfig.Instance.Placeables)
+                    {
+                        collection.AddRange(new List<int>
+                        {
+                            ModContent.ItemType<HarvesterRelicItem>(),
+                            ModContent.ItemType<HarvesterTrophyItem>()
+                        });
+                    }
+
+                    if (AConfigurationConfig.Instance.VanityArmor)
+                    {
+                        collection.AddRange(new List<int>
+                        {
+                            ModContent.ItemType<SoulHarvesterMask>()
+                        });
+                    }
+
+                    List<int> loot = new List<int>
+                    {
+                        summonItem,
+                        ModContent.ItemType<CaughtDungeonSoulFreed>(),
+                    };
+
+                    if (AConfigurationConfig.Instance.Accessories)
+                    {
+                        loot.AddRange(new List<int>
+                        {
+                            ModContent.ItemType<SigilOfRetreat>(),
+                            ModContent.ItemType<SigilOfEmergency>(),
+                            ModContent.ItemType<SigilOfPainSuppression>()
+                        });
+                    }
+
+                    bossChecklist.Call(
+                        "AddMiniBoss",
+                        5.1f,
+                        ModContent.NPCType<Harvester>(),
+                        this,
+                        Harvester.name,
+                        (Func<bool>)(() => AssWorld.downedHarvester),
+                        summonItem,
+                        collection,
+                        loot,
+                        $"Use a [i:{summonItem}] in the dungeon after Skeletron has been defeated",
+                        null,
+                        $"{this.Name}/NPCs/DungeonBird/HarvesterPreview"
+                    );
+                }
+                else
+                {
+                    bossChecklist.Call("AddMiniBossWithInfo", Harvester.name, 5.1f, (Func<bool>)(() => AssWorld.downedHarvester), "Use a [i:" + ModContent.ItemType<IdolOfDecay>() + "] in the dungeon after Skeletron has been defeated");
+                }
+            }
+
+            if (ModLoader.TryGetMod("SummonersAssociation", out Mod summonersAssociation) && summonersAssociation.Version > new Version(0, 4, 1))
+            {
+                if (AConfigurationConfig.Instance.Bosses && AConfigurationConfig.Instance.Weapons)
+                {
+                    summonersAssociation.Call("AddMinionInfo", ModContent.ItemType<EverglowLantern>(), ModContent.BuffType<CompanionDungeonSoulMinionBuff>(), new List<int>
+                    {
+                        ModContent.ProjectileType<CompanionDungeonSoulPreWOFMinion>(),
+                    });
+
+                    summonersAssociation.Call("AddMinionInfo", ModContent.ItemType<EverhallowedLantern>(), ModContent.BuffType<CompanionDungeonSoulMinionBuff>(), new List<int>
+                    {
+                        ModContent.ProjectileType<CompanionDungeonSoulPostWOFMinion>(),
+                        ModContent.ProjectileType<CompanionDungeonSoulFrightMinion>(),
+                        ModContent.ProjectileType<CompanionDungeonSoulMightMinion>(),
+                        ModContent.ProjectileType<CompanionDungeonSoulSightMinion>()
+                    });
+                }
+
+                if (AConfigurationConfig.Instance.Weapons)
+                {
+                    summonersAssociation.Call("AddMinionInfo", ModContent.ItemType<SlimeHandlerKnapsack>(), ModContent.BuffType<SlimePackMinionBuff>(), ModContent.ProjectileType<SlimePackMinion>());
+                    List<int> drones = new List<int>();
+                    foreach (var drone in DroneController.DataList)
+                    {
+                        drones.Add(drone.ProjType);
+                    }
+
+                    summonersAssociation.Call("AddMinionInfo", ModContent.ItemType<DroneController>(), ModContent.BuffType<DroneControllerBuff>(), drones);
+                }
+            }
+        }
+    }
+}
