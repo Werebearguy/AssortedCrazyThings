09ca979506b221bea3d627c6c168776384368dc2
direwolf420 <amakejk1@web.de>
2022-04-14 22:37:18 +0200

    make paintings obtainable

--- Items/Placeable/Paintings/SCP3515Item.cs
+++ Items/Placeable/Paintings/SCP3515Item.cs
@@ -1,12 +1,14 @@
 using AssortedCrazyThings.Tiles.Paintings;
+using Terraria.ID;
 
 namespace AssortedCrazyThings.Items.Placeable.Paintings
 {
-	//TODO aquisition
 	public class SCP3515Item : PaintingItemBase<SCP3515>
 	{
 		public override string PaintingName => "SCP-3515";
 
 		public override string PaintingAuthor => "Unknown";
+
+		public override (int item, int amount) RecipeIngredient => (ItemID.DirtBlock, 1);
 	}
 }
