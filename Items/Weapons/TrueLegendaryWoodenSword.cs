b9e41322de18994bf50b95103561bf92e390815a
direwolf420 <amakejk1@web.de>
2022-07-17 19:33:46 +0200

    fix true legendary wooden sword projectile damage not scaling with weapon damage

--- Items/Weapons/TrueLegendaryWoodenSword.cs
+++ Items/Weapons/TrueLegendaryWoodenSword.cs
@@ -9,8 +9,6 @@ namespace AssortedCrazyThings.Items.Weapons
 {
 	public class TrueLegendaryWoodenSword : WeaponItemBase
 	{
-		public int ProjDamage = 28; //Default fallback
-
 		public override void SafeSetStaticDefaults()
 		{
 			DisplayName.SetDefault("True Legendary Wooden Sword");
@@ -20,7 +18,6 @@ namespace AssortedCrazyThings.Items.Weapons
 		public override void SetDefaults()
 		{
 			Item.CloneDefaults(ItemID.CobaltSword);
-			ProjDamage = (int)(Item.damage * 0.8f);
 			Item.width = 58;
 			Item.height = 58;
 			Item.rare = 7;
@@ -46,7 +43,7 @@ namespace AssortedCrazyThings.Items.Weapons
 
 		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
 		{
-			Projectile.NewProjectile(source, player.Center + Vector2.Normalize(velocity) * 30f, velocity, type, ProjDamage, knockback, Main.myPlayer);
+			Projectile.NewProjectile(source, player.Center + Vector2.Normalize(velocity) * 30f, velocity, type, (int)(damage * 0.8f), knockback, Main.myPlayer);
 			return false;
 		}
 	}
