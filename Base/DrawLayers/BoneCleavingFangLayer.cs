39ecd3a063804a8bf38e274c5895e77d3f7bc363
direwolf420 <amakejk1@web.de>
2023-03-30 22:02:49 +0200

    bone-cleaving fang

---
+++ Base/DrawLayers/BoneCleavingFangLayer.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using ReLogic.Content;
+using AssortedCrazyThings.Items.Weapons;
+
+namespace AssortedCrazyThings.Base.DrawLayers
+{
+	//TODO convert this to generic item glowmask one + func with DrawLayerData
+	[Content(ContentType.Bosses)]
+	public sealed class BoneCleavingFangLayer : AssPlayerLayer
+	{
+		private static Asset<Texture2D> glowmask;
+
+		public override void SetStaticDefaults()
+		{
+			glowmask = ModContent.Request<Texture2D>(ModContent.GetInstance<BoneCleavingFang>().Texture + "Glowmask");
+		}
+
+		public override void Unload()
+		{
+			glowmask = null;
+		}
+
+		public override Position GetDefaultPosition()
+		{
+			//replicate vanilla placement, HeldItem will be replaced
+			//return new Multiple()
+			//{
+			//	{ new Between(PlayerDrawLayers.BalloonAcc, PlayerDrawLayers.Skin), drawinfo => drawinfo.weaponDrawOrder == WeaponDrawOrder.BehindBackArm },
+			//	{ new Between(PlayerDrawLayers.SolarShield, PlayerDrawLayers.ArmOverItem), drawinfo => drawinfo.weaponDrawOrder == WeaponDrawOrder.BehindFrontArm },
+			//	{ new Between(PlayerDrawLayers.BladedGlove, PlayerDrawLayers.ProjectileOverArm), drawinfo => drawinfo.weaponDrawOrder == WeaponDrawOrder.OverFrontArm }
+			//};
+			return new AfterParent(PlayerDrawLayers.HeldItem);
+		}
+
+		public override bool GetDefaultVisibility(PlayerDrawSet drawInfo)
+		{
+			Player drawPlayer = drawInfo.drawPlayer;
+			Item heldItem = drawInfo.heldItem;
+			if (!BoneClearingFangPlayer.HoldingItem(heldItem))
+			{
+				return false;
+			}
+			bool usingItem = drawPlayer.itemAnimation > 0 && heldItem.useStyle != ItemUseStyleID.None;
+			bool holdingSuitableItem = heldItem.holdStyle != 0 && !drawPlayer.pulley;
+
+			if (!drawPlayer.CanVisuallyHoldItem(heldItem))
+			{
+				holdingSuitableItem = false;
+			}
+
+			if (drawInfo.shadow != 0f || drawPlayer.JustDroppedAnItem || drawPlayer.frozen || !(usingItem || holdingSuitableItem) || heldItem.type <= 0 || drawPlayer.dead || heldItem.noUseGraphic || drawPlayer.wet && heldItem.noWet)
+			{
+				return false;
+			}
+
+			return true;
+		}
+
+		protected override void Draw(ref PlayerDrawSet drawInfo)
+		{
+			Player drawPlayer = drawInfo.drawPlayer;
+
+			//Copied crucial logic from vanilla since we hide the layer
+			//if (drawInfo.drawPlayer.heldProj >= 0 && drawInfo.shadow == 0f && !drawInfo.heldProjOverHand)
+			//{
+			//	drawInfo.projectileDrawPosition = drawInfo.DrawDataCache.Count;
+			//}
+			//drawInfo.itemColor = Lighting.GetColor((int)(drawInfo.Position.X + drawInfo.drawPlayer.width * 0.5) / 16, (int)((drawInfo.Position.Y + drawInfo.drawPlayer.height * 0.5) / 16.0));
+
+			var color = new Color(250, 250, 250, drawInfo.heldItem.alpha);
+			BoneClearingFangPlayer modPlayer = drawPlayer.GetModPlayer<BoneClearingFangPlayer>();
+			float alpha = modPlayer.TimerRatio;
+
+			DrawWithTextureAndColor(drawInfo, glowmask.Value, color * alpha);
+		}
+
+		private static void DrawWithTextureAndColor(PlayerDrawSet drawInfo, Texture2D texture, Color? color = null)
+		{
+			Player drawPlayer = drawInfo.drawPlayer;
+			Item heldItem = drawInfo.heldItem;
+			int useStyle = heldItem.useStyle;
+			float adjustedItemScale = drawPlayer.GetAdjustedItemScale(heldItem);
+			Vector2 position = new Vector2((int)(drawInfo.ItemLocation.X - Main.screenPosition.X), (int)(drawInfo.ItemLocation.Y - Main.screenPosition.Y));
+			Rectangle? sourceRect = new Rectangle(0, 0, texture.Width, texture.Height);
+
+			if (useStyle == ItemUseStyleID.Swing)
+			{
+				Vector2 origin = new Vector2(drawPlayer.direction == -1 ? texture.Width : 0, drawPlayer.gravDir == -1 ? 0 : texture.Height);
+				DrawData drawData = new DrawData(texture, position, sourceRect, heldItem.GetAlpha(color ?? drawInfo.itemColor), drawPlayer.itemRotation, origin, adjustedItemScale, drawInfo.itemEffect, 0);
+				drawInfo.DrawDataCache.Add(drawData);
+			}
+		}
+	}
+}
---
+++ Projectiles/Weapons/BoneCleavingFangProj.cs
@@ -0,0 +1,106 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AssortedCrazyThings.Projectiles.Weapons
+{
+	[Content(ContentType.Bosses)]
+	public class BoneCleavingFangProj : AssProjectile
+	{
+		public ref float Power => ref Projectile.ai[0];
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Bone-Cleaving Fang");
+			ProjectileID.Sets.TrailCacheLength[Projectile.type] = 6;
+			ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.height = 28;
+			Projectile.width = 28;
+			Projectile.aiStyle = -1;
+			Projectile.alpha = 150;
+			Projectile.penetrate = 4;
+			Projectile.timeLeft = 240;
+			Projectile.tileCollide = true;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Melee;
+			Projectile.ignoreWater = true;
+
+			DrawOffsetX = -6;
+			DrawOriginOffsetY = -10;
+		}
+
+		public override void ModifyDamageHitbox(ref Rectangle hitbox)
+		{
+			hitbox.Inflate(6, 6);
+		}
+
+		public override void PostAI()
+		{
+			if (Projectile.alpha > 0)
+			{
+				Projectile.alpha -= 25;
+				if (Projectile.alpha < 0)
+				{
+					Projectile.alpha = 0;
+				}
+			}
+
+			Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+		}
+
+		public override bool PreDraw(ref Color lightColor)
+		{
+			Texture2D texture2D = TextureAssets.Projectile[Projectile.type].Value;
+			Vector2 drawOrigin = new Vector2((texture2D.Width - Projectile.width) / 2 + Projectile.width / 2, Projectile.height / 2);
+			for (int k = Projectile.oldPos.Length - 1; k > 0; k--)
+			{
+				Vector2 drawPos = Projectile.oldPos[k] - Main.screenPosition + drawOrigin + new Vector2(DrawOffsetX, Projectile.gfxOffY);
+				Color color = Color.White * ((Projectile.oldPos.Length - k) / (float)Projectile.oldPos.Length);
+				Main.EntitySpriteDraw(texture2D, drawPos, null, color, Projectile.oldRot[k], drawOrigin - new Vector2(DrawOriginOffsetX, DrawOriginOffsetY), Projectile.scale, SpriteEffects.None, 0);
+			}
+			return true;
+		}
+
+		public override void AI()
+		{
+			if (Projectile.localAI[0] == 0f)
+			{
+				Projectile.localAI[0] = 1f;
+
+				SoundEngine.PlaySound(SoundID.Item43, Projectile.Center);
+			}
+
+			Lighting.AddLight(Projectile.Center, 0.05f, 0.1f, 0.45f);
+
+			for (int i = 0; i < Power * 4; i++)
+			{
+				Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, 135, 0f, 0f, 150);
+				dust.noGravity = true;
+				dust.velocity *= 1.5f;
+				dust.velocity += 2.5f * Projectile.velocity * 0.4f;
+				dust.scale = 1.5f;
+			}
+		}
+
+		public override void Kill(int timeLeft)
+		{
+			Vector2 normalizedVelocity = Projectile.velocity.SafeNormalize(Vector2.Zero);
+			for (int i = 0; i < 20; i++)
+			{
+				Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, 135, 0f, 0f, 150);
+				dust.noGravity = true;
+				dust.velocity *= 1.5f;
+				dust.velocity += 2.5f * normalizedVelocity;
+				dust.scale = 1.5f;
+			}
+		}
+	}
+}
