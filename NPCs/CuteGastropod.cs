77ac019e084780ffffd5999c3da9b9d441a983f2
direwolf420 <amakejk1@web.de>
2023-05-18 11:58:07 +0200

    revert overworld hallow daytime bestiary

--- NPCs/CuteGastropod.cs
+++ NPCs/CuteGastropod.cs
@@ -68,7 +68,6 @@ namespace AssortedCrazyThings.NPCs
 			bestiaryEntry.UIInfoProvider = new CommonEnemyUICollectionInfoProvider(ContentSamples.NpcBestiaryCreditIdsByNpcNetIds[NPC.type], quickUnlock: true);
 
 			bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[] {
-				BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Times.DayTime,
 				BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.TheHallow,
 			});
 		}
