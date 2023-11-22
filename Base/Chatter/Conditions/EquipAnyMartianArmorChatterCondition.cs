46132a33ae1d2278cea4604f8a939dc8d8c45c17
direwolf420 <amakejk1@web.de>
2023-11-22 13:32:50 +0100

    typo, thorium armor check

--- Base/Chatter/Conditions/EquipAnyMartianArmorChatterCondition.cs
+++ Base/Chatter/Conditions/EquipAnyMartianArmorChatterCondition.cs
@@ -1,5 +1,6 @@
 ﻿using AssortedCrazyThings.Base.Data;
 using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace AssortedCrazyThings.Base.Chatter.Conditions
 {
@@ -7,9 +8,20 @@ namespace AssortedCrazyThings.Base.Chatter.Conditions
 	{
 		protected override bool Check(ChatterSource source, IChatterParams param)
 		{
-			//TODO thorium conduit armor?
-			return JustEquipped(param, new EquipSnapshot(ArmorIDs.Head.MartianCostumeMask, ArmorIDs.Body.MartianCostumeShirt, ArmorIDs.Legs.MartianCostumePants)) ||
+			bool ret = JustEquipped(param, new EquipSnapshot(ArmorIDs.Head.MartianCostumeMask, ArmorIDs.Body.MartianCostumeShirt, ArmorIDs.Legs.MartianCostumePants)) ||
 				JustEquipped(param, new EquipSnapshot(ArmorIDs.Head.MartianUniformHelmet, ArmorIDs.Body.MartianUniformTorso, ArmorIDs.Legs.MartianUniformPants));
+
+			if (!ret && ModLoader.TryGetMod("ThoriumMod", out var mod))
+			{
+				if (EquipLoader.GetEquipSlot(mod, "ConduitHelmet", EquipType.Head) is int head && head > 0 &&
+					EquipLoader.GetEquipSlot(mod, "ConduitSuit", EquipType.Body) is int body && body > 0 &&
+					EquipLoader.GetEquipSlot(mod, "ConduitLeggings", EquipType.Legs) is int legs && legs > 0)
+				{
+					return JustEquipped(param, new EquipSnapshot(head, body, legs));
+				}
+			}
+
+			return ret;
 		}
 	}
 }
